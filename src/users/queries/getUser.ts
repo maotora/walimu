de2485c376784a0fad3ff7575c8290a401e98bd4
Maotora ᕙ(⇀‸↼‶)ᕗ <maotoramakweba@live.com>
2023-08-27 11:03:56 +0300

    chore: both users can see each other after pairing

--- src/users/queries/getUser.ts
+++ src/users/queries/getUser.ts
@@ -5,7 +5,7 @@ import { z } from "zod"
 import { Ctx } from "blitz"
 
 const GetUser = z.object({
-  id: z.number().optional().refine(Boolean, "Required"),
+  id: z.number().refine(Boolean, "Required"),
 })
 
 export default resolver.pipe(
@@ -16,6 +16,47 @@ export default resolver.pipe(
 
     if (id !== userId) {
       //- If viewing someone else info
+      const [myPost] = await db.post.findMany({
+        where: { userId: userId! },
+      })
+
+      if (myPost) {
+        const pairInfo = await db.watcher.findUnique({
+          where: {
+            watcherUniquness: {
+              userId: id,
+              postId: myPost?.id,
+            },
+          },
+        })
+
+        if (pairInfo?.approved) {
+          //- If pair approved both can see each other's details
+          const user = await db.user.findFirst({
+            where: { id },
+            select: {
+              id: true,
+              name: true,
+              role: true,
+              phone: true,
+              email: true,
+              gender: true,
+              currentSchool: {
+                select: {
+                  name: true,
+                  type: true,
+                  location: true,
+                },
+              },
+            },
+          })
+
+          if (!user) throw new NotFoundError()
+
+          return user
+        }
+      }
+
       const user = await db.user.findFirst({
         where: { id },
         select: {
